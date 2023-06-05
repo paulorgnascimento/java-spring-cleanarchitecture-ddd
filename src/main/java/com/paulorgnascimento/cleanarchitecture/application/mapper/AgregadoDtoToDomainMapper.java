2bee4f9e7786748cfd1a826c48a7126257ce4d3f
paulorgnascimento <124225900+paulorgnascimento@users.noreply.github.com>
2023-06-04 23:31:10 -0300

    test

--- src/main/java/com/paulorgnascimento/cleanarchitecture/application/mapper/AgregadoDtoToDomainMapper.java
+++ src/main/java/com/paulorgnascimento/cleanarchitecture/application/mapper/AgregadoDtoToDomainMapper.java
@@ -1,29 +1,24 @@
 package com.paulorgnascimento.cleanarchitecture.application.mapper;
 
 import com.paulorgnascimento.cleanarchitecture.application.dto.AgregadoInDto;
-import com.paulorgnascimento.cleanarchitecture.application.dto.EntidadeInDto;
 import com.paulorgnascimento.cleanarchitecture.domain.aggregateroot.Agregado;
-import com.paulorgnascimento.cleanarchitecture.domain.entity.Entidade;
 import com.paulorgnascimento.cleanarchitecture.domain.valueobject.ObjetoDeValor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface AgregadoDtoToDomainMapper {
 
-    AgregadoDtoToDomainMapper INSTANCE = Mappers.getMapper( AgregadoDtoToDomainMapper.class );
+    AgregadoDtoToDomainMapper INSTANCE = Mappers.getMapper(AgregadoDtoToDomainMapper.class);
 
     @Mapping(source = "campo1", target = "campo1")
-    @Mapping(source = "campo2", target = "campo2")
-    Agregado toAgregado(AgregadoInDto agregadoInDto);
+    @Mapping(source = "campo2", target = "campo2", qualifiedByName = "stringToObjetoDeValor")
+    Agregado fromDTO(AgregadoInDto agregadoInDto);
 
-    List<Entidade> toEntityList(List<EntidadeInDto> entidadeDtoList);
-
-    @Mapping(target = "campo2", ignore = true) // Since we don't have a way to convert String to ObjetoDeValor
-    Entidade toEntity(EntidadeInDto entidadeDto);
-
-    ObjetoDeValor toObjetoDeValor(String campo2);
+    @Named("stringToObjetoDeValor")
+    default ObjetoDeValor stringToObjetoDeValor(String campo2) {
+        return new ObjetoDeValor(campo2);
+    }
 }
--- src/main/java/com/paulorgnascimento/cleanarchitecture/domain/valueobject/ObjetoDeValor.java
+++ src/main/java/com/paulorgnascimento/cleanarchitecture/domain/valueobject/ObjetoDeValor.java
@@ -2,13 +2,13 @@ package com.paulorgnascimento.cleanarchitecture.domain.valueobject;
 
 public class ObjetoDeValor {
 
-    private String campo2;
+    private String campo2vo;
 
-    public ObjetoDeValor(String campo2) {
-        this.campo2 = campo2;
+    public ObjetoDeValor(String campo2vo) {
+        this.campo2vo = campo2vo;
     }
 
-    public String getCampo1() { return campo2; }
+    public String getCampo1() { return campo2vo; }
 
     // Value Objects geralmente são imutáveis e, assim, não incluem setters e não tem uma identidade única
 }
